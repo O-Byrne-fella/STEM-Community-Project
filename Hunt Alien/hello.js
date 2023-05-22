740506498dae2fe789bfc2f2960bc5df89f2b177
O-Byrne-fella <C19482842@mytudublin.ie>
2023-05-22 11:24:42 +0100

    New changes

--- Hunt Alien/hello.js
+++ Hunt Alien/hello.js
@@ -12,10 +12,14 @@
     inputValidator: (result) => {
       return !result && 'You need to agree with T&C';
     },
-    footer: '<a href=file:///C:/Users/seany/OneDrive%20-%20Technological%20University%20Dublin/DT021%20yr3/STEM/Hunt%20Alien/STEM%20Outreach%20Project%20Description.pdf target="_blank">View Terms and Conditions (PDF)</a>',
+    footer: '<a href="file:///C:/Users/seany/OneDrive%20-%20Technological%20University%20Dublin/DT021%20yr3/STEM/Hunt%20Alien/STEM%20Outreach%20Project%20Description.pdf" target="_blank">View Terms and Conditions (PDF)</a>',
   });
 
   if (accept) {
-    Swal.fire('You agreed with T&C :)');
+    Swal.fire({
+      title: 'You agreed with T&C :)',
+      html: '<img src="C:\Users\seany\OneDrive - Technological University Dublin\DT021 yr3\STEM\Hunt Alien\8756025_AdobeExpress.gif" alt="Agreed GIF" style="width: 200px;">', // Replace with the URL of your GIF and adjust the width as needed
+      confirmButtonText: 'OK',
+    });
   }
 })();
